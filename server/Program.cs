fd7a1cdc8572030e08a7aac88b5c37f3158b7457
Nikitakorovinskii <78597438+Nikitakorovinskii@users.noreply.github.com>
2022-05-28 10:42:46 +0300

    Добавленние вход но с авторизацией ошибка

--- server/Program.cs
+++ server/Program.cs
@@ -3,8 +3,10 @@ using Nancy;
 using Nancy.Hosting.Self;
 using Nancy.ModelBinding;
 using NancyNew;
+using server;
 using server.BdTable;
 using Server;
+using System.Linq;
 
 class Program
 {
@@ -38,6 +40,13 @@ public class HomeModule : NancyModule
     {
        Auth human = new Auth();
        human = new() { Login = "Anthony" };
+        Get("/cars", args => 
+        {
+            using (work100013Context db = new())
+            {
+                List<Car> car = db.Cars.ToList();
+            }
+        });
         Get("/", args => "Олег лучший парень на деревне");
         Get("/SayHello", args => $"Hello {this.Request.Query["name"]}");
         Get("/SayHello2/{name}", args => $"Hello {args.name}");
@@ -61,34 +70,79 @@ public class HomeModule : NancyModule
         });
         Post("/authorization", (x) =>
         {
-            Client infoHuman = new Client();
-            Auth client = new Auth();
+           
             x = this.Bind<Client>();
-            dynamic c = this.Bind<Auth>();
+            
 
             var res = new Response();
 
+            using (work100013Context db = new())
+            {
+                string pasID = x.Passport;
+                int idClient = 0;
+                Client client = new Client
+                {
+                    Name = x.Name,
+                    LastName = x.LastName,
+                    MiddleName = x.MiddleName,
+                    DataOfBith = x.DataOfBith,
+                    Number = x.Number,
+                    Passport = x.Passport,
+                    NumberDriver = x.NumberDriver,
+                };
+                db.Clients.Add(client);
+                db.SaveChanges();
+                var list = db.Clients.Where(p => p.Passport == pasID);
+                foreach (var u in list)
+                {
+                    idClient = u.IdClient;
+                }
+                Auth auth = new Auth
+                {
+                    Login = x.Number,
+                    Email = x.Email,
+                    Password = x.Password,
+                    IdClient = idClient
+
+                };
+                db.Auths.Add(auth);
+                db.SaveChanges();
+                Console.WriteLine("Efdmspafm");
+            }
             res.StatusCode = (HttpStatusCode)200;
             res.Headers["Access-Control-Allow-Origin"] = "*";
             res.Headers["Access-Control-Allow-Method"] = "POST";
             res.Headers["Access-Control-Expose-Headers"] = "Human";
             res.Headers["Content-Type"] = "application/json";
-             res.Headers["Human"] = System.Text.Json.JsonSerializer.Serialize(human);
-            Console.WriteLine(c.password);
+            res.Headers["Human"] = System.Text.Json.JsonSerializer.Serialize(human);
+
+           
+          
             return res;
         });
-        Post("/auth", async (x) =>
+        Post("/auth", (x) =>
         {
-           x = this.Bind<Auth>();
-           Auth acc;
+            x = this.Bind<Auth>();
             //TODO: Add JWT
             string login = x.Login;
             string password = x.Password;
+            int x1 = 0;
+            using (work100013Context db = new())
+            {
+               var acc1 = db.Auths.Where(l => l.Login == login && l.Password == password);
+                foreach(var u in acc1)
+                {
+                    if (acc1 != null)
+                    {
+                        x1++;
+                    }
+                }
+            }
 
-     
-        Response response = new();
+            Response response = new();
 
-      
+            if (x1 != 0)
+            {
 
                 response.StatusCode = HttpStatusCode.OK;
                 response.Headers["Access-Control-Allow-Origin"] = "*";
@@ -99,10 +153,14 @@ public class HomeModule : NancyModule
                 response.Headers["Access-Control-Expose-Headers"] = "Token, Account";
                 response.Headers["Content-Type"] = "application/json";
 
-            Console.WriteLine(x.Login + " " + x.Password);
-            return response;
-            
-           
+                response.Headers["Account"] = System.Text.Json.JsonSerializer.Serialize(x1);
+                return response;
+            }
+            else
+            {
+                response.StatusCode = HttpStatusCode.NotFound;
+                return response;
+            }
         });
     }
 }
